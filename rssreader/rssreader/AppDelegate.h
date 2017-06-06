5c952b0c79100b499987b316eaad196c41b8e2a4
keepcalmandcodecodecode <igor.github@gmail.com>
2017-06-06 22:17:32 +0300

    create base project

---
+++ rssreader/rssreader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rssreader
+//
+//  Created by developer on 6/6/17.
+//  Copyright © 2017 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rssreader/rssreader/Presentation/RSSFeedVC.h
@@ -0,0 +1,13 @@
+//
+//  RSSFeedVC.h
+//  rssreader
+//
+//  Created by developer on 6/6/17.
+//  Copyright © 2017 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSSFeedVC : UIViewController
+
+@end
